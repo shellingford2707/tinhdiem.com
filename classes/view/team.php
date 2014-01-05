d7935169381109135acccb9ff3adfc639f3171cd
hoangkianh <hoangkianh31592@gmail.com>
2014-01-06 01:30:34 +0700

    cập nhật giao diện phần 'team'

--- classes/view/team.php
+++ classes/view/team.php
@@ -2,5 +2,57 @@
     <div class="container">
         <div class="align"><i class="fa fa-users"></i></div>
         <h1>Team</h1>
+        <div class="row">
+            <div class="span2 offset3">
+                <div class="teamalign"> 
+                    <div class="overlay" id="hka">
+                        <div class="info">
+                            <a href="https://www.facebook.com/hoangkianh31592" target="_blank"><i class="fa fa-facebook fa-2x"></i></a>
+                        </div>
+                    </div>
+                    <img class="team-thumb img-circle" src="img/hka.jpg" alt="HKA"> 
+                </div>
+                <h3>Hoàng Kì Anh</h3>
+                <div class="job-position">cầm đầu <i class="fa fa-meh-o fa-2x"></i></div>
+            </div>
+            <div class="span2">
+                <div class="teamalign"> 
+                    <div class="overlay" id="orion">
+                        <div class="info">
+                            <a href="https://www.facebook.com/orioncake109" target="_blank"><i class="fa fa-facebook fa-2x"></i></a>
+                        </div>
+                    </div>
+                    <img class="team-thumb img-circle" src="img/orioncustas.jpg" alt="orioncustas"> 
+                </div>
+                <h3>Orion Custas</h3>
+                <div class="job-position">táy máy database <i class="fa fa-bug fa-2x"></i></div>
+            </div>
+            <div class="span2">
+                <div class="teamalign">
+                    <div class="overlay" id="vynk">
+                        <div class="info">
+                            <a href="https://www.facebook.com/vinh2888" target="_blank"><i class="fa fa-facebook fa-2x"></i></a>
+                        </div>
+                    </div>
+                    <img class="team-thumb img-circle" src="img/vynkceleron.jpg" alt="vynkceleron"> 
+                </div>
+                <h3>Vynk Celeron</h3>
+                <div class="job-position">mới học php <span><i class="fa fa-cog fa-2x fa-spin"></i></div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="span10 offset1">
+                <hr class="featurette-divider">
+                <div class="featurette">
+                    <h2 class="featurette-heading">Bạn muốn biết rõ hơn? <span class="muted">| dưới đây là 1 chút về chúng tôi</span></h2>
+                    <p>Nhóm xây dựng website này được 'cầm đầu' bởi Nguyễn Hoàng Anh (CNPM K51) cộng thêm sự hợp tác của 2 bạn Nguyễn Việt Anh & Phạm Đình Vinh (CNPM K52).</p>
+                    <p>Chúng tôi rất hoan nghênh những ý kiến đóng góp của bạn để website <a href="#">tinhdiem.com</a> ngày một hoàn thiện hơn.</p>
+                    <p>Hãy gửi ý kiến của bạn về facebook của chúng tôi (click vào ảnh nhé) <i class="fa fa-smile-o"></i></p>
+                </div>
+                <!-- /.featurette -->
+                <hr class="featurette-divider">
+            </div>
+            <!-- .span10 -->
+        </div>
     </div>
 </section>
