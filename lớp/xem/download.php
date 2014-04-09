4d0f8c3c66a6f0d75995debf51b183d2dacf4b48
shellingford2707 <buiquangtu1992@gmailcom>
2014-04-10 00:57:16 +0800

    Update and rename classes/view/download.php to lớp/xem/download.php

---
+++ lớp/xem/download.php
@@ -0,0 +1,138 @@
+<section id="download" class="single-page scrollblock">
+    <div class="container">
+        <div class="align"><i class="fa fa-download"></i></div>	VKT06.3	Vẽ kỹ thuật	3	7.5	6.7	6.9	C+
+2	KXD14.2	Thị trường chứng khoán	2	8.0	4.0	5.2	D+
+3	KVT05.2	Tài chính tiền tệ	2	9.0
+8.5	1.0
+7.5	3.4
+7.8	F+
+B
+4	GDT01.1	Giáo dục thể chất F1	1	3.0
+4.0	5.0|5.0
+4.0|7.0	4.4|4.4
+4.0|6.1	D|D
+D|C
+5	KXD02.2	Quản trị kinh doanh	2	7.0	3.0	4.2	D
+6	QLY06.2	Khoa học quản lý	2	7.0	5.0	5.6	C
+7	DBO14.3	Xây dựng đường ô tô	3	10	7.0	7.9	B
+8	CAU14.3	Công trình nhân tạo F2	3	9.0	5.0	6.2	C
+9	KXD03.2	Điều tra quy hoạch	2	3.0
+7.0	2.0|3.0
+6.0	2.3|3.0
+6.3	F|F+
+C
+10	KXD04.3	Kinh tế xây dựng	3	4.0
+
+8.0	3.0|2.0
+
+4.0	3.3|2.6
+
+5.2	F+|F
+
+D+
+11	GIT07.2	Toán kinh tế	2	5.0
+9.0	4.5
+8.0	4.7
+8.3	D
+B+
+12	MLN02.3	Những nguyên lý cơ bản của chủ nghĩa MLN F2	3	9.0	2.0	4.1	D
+13	GQP02.2	Giáo dục quốc phòng F2	2	6.0
+7.0	4.0|3.0
+6.0	4.6|3.9
+6.3	D|F+
+C
+14	MLN.TC0001.2	Những nguyên lý cơ bản của chủ nghĩa MLN F1	2	7.0	4.0	4.9	D
+15	SBV04.2	Sức bền vật liệu	2	8.0	10	9.4	A
+16	KXD13.2	Marketing trong xây dựng	2	10	8.0	8.6	A
+17	GQP01.2	Giáo dục quốc phòng F1	2	4.0
+6.0	5.0|5.0
+4.0|6.0	4.7|4.7
+4.6|6.0	D|D
+D|C
+18	GIT05.2	Giải tích	2	6.0	2.0|4.5	3.2|5.0	F+|D+
+19	QLY10.2	Nguyên lý thống kê	2	5.0	5.0	5.0	D+
+20	KVT02.3	Kinh tế vĩ mô	3	8.0	3.5	4.9	D
+21	TRD02.2	Trắc địa đại cương	2	7.0	2.8	4.1	D
+22	GDT05.1	Giáo dục thể chất F5	1	5.0	3.0|5.0	3.6|5.0	F+|D+
+23	KVT03.3	Kinh tế lượng	3	8.0	9.0	8.7	A
+24	KXD06.4	Định mức kỹ thuật và định giá sản phẩm xây dựng	4	7.0	1.0|3.5	2.8|4.6	F|D
+25	DSO01.2	Đại số tuyến tính	2	5.0	3.0|5.0	3.6|5.0	F+|D+
+26	QLY.TC02.2	Quản lý nhà nước về kinh tế	2	8.0	3.0	4.5	D
+27	KCA07.2	Cơ kết cấu	2	4.0
+5.0
+7.0	0.5|0.0
+0.0|0.0
+5.0	1.6|1.2
+1.5|1.5
+5.6	F|F
+F|F
+C
+28	VLX03.2	Vật liệu xây dựng	2	8.0	8.0	8.0	B+
+29	CAU13.3	Công trình nhân tạo F1	3	6.0	4.0	4.6	D
+30	KCA08.3	Kết cấu công trình	3	8.0	2.0|6.0	3.8|6.6	F+|C+
+31	KXD01.2	Thực tập kỹ thuật	2	6.5	6.5	6.5	C+
+32	KXD12.2	Thống kê đầu tư xây dựng	2	8.0
+8.0	1.0|2.0
+6.0	3.1|3.8
+6.6	F+|F+
+C+
+33	KXD07.4	Kế toán xây dựng cơ bản	4	7.0	5.0	5.6	C
+34	KXD09.3	Phân tích HĐKT DNXD	3	8.5	5.0	6.1	C
+35	KXD16.2	Lập và phân tích dự án đầu tư	2	9.5	7.0	7.8	B
+36	KXD10.4	Thực tập tốt nghiệp	4	7.5	7.5	7.5	B
+37	VLY.TC04.2	Vật lý	2	6.0	5.0	5.3	D+
+38	DCS01.3	Đường lối cách mạng của Đảng CSVN	3	6.0	4.0	4.6	D
+39	HOA01.2	Hóa Học	2	7.0	7.0	7.0	B
+40	MXD33.2	Máy xây dựng	2	9.0	5.0	6.2	C
+41	KVT.TC01.3	Kinh tế vi mô	3	7.0	4.0	4.9	D
+42	GDT02.1	Giáo dục thể chất F2	1	6.0	6.0	6.0	C
+43	GQP03.2	Giáo dục quốc phòng F3	2	9.0	6.0	6.9	C+
+44	CPM.TC02.3	Tin học đại cương	3	10	6.0	7.2	B
+45	HCM01.2	Tư tưởng Hồ Chí Minh	2	8.0	4.0	5.2	D+
+46	QLY03.2	Pháp luật kinh tế	2	3.0	6.0	5.1	D+
+47	KXD11.2	Tài chính doanh nghiệp	2	7.0	2.0|5.0	3.5|5.6	F+|C
+48	ANH01.3	Tiếng Anh F1	3	8.5	3.5	5.0	D+
+49	QLY01.2	Pháp luật đại cương	2	5.0	6.0	5.7	C
+50	DKT17.3	Địa kỹ thuật	3	8.0	5.3	6.1	C
+51	DBO13.3	Thiết kế đường	3	8.4	4.0	5.3	D+
+52	KXD08.4	Tổ chức và quản lý thi công xây dựng	4	9.0	7.5	8.0	B+
+53	ANH03.3	Tiếng anh F2	3	7.5	5.3	6.0	C
+54	GQP04.1	Giáo dục quốc phòng F4	1	8.0	5.0	5.9	C
+55	GDT03.1	Giáo dục thể chất F3	1	7.0	6.0	6.3	C
+56	GDT04.1	Giáo dục thể chất F4	1	8.0	5.0	5.9	C
+57	KXD05.3	Chiến lược kinh doanh và kê hoạch hóa	3	9.0	3.0	4.8	D
+        <h1>Download</h1>
+        <div class="featurette">
+            <h2 class="featurette-heading">Hiện tại có 2 phiên bản phần mềm tính điểm</h2>
+            <p>Chúng tôi đang cố gắng xây dựng phần mềm hoàn thiện nhất <i class="fa fa-smile-o"></i></p>
+        </div>
+        <div class="row" id="step4">
+            <article class="span4 offset2">
+                <div class="mask2"> <a href="img/sc.png" rel="prettyPhoto"><img class="img-news" src="img/sc.png" alt="Score Caculator"></a></div>
+                <div class="inside">
+                    <hgroup><h2>Score Caculator</h2></hgroup>
+                    <div class="entry-content">
+                        <p>Tác giả: HKA</p>
+                        <a href="https://app.box.com/s/ggzec60xhxxzz3moayf7" target="_blank" class="download-link">tải về <i class="fa fa-cloud-download"></i></a>
+                        <p>Bên cạnh tính điểm tích lũy, phần mềm có các chức năng như xuất Excel, thống kê biểu đồ.
+                            Giao diện đẹp, thân thiện, tự động cập nhật.</p>
+                    </div>
+                </div>
+            </article>
+            <article class="span4">
+                <div class="mask2"> <a href="img/UTCpoint.png" rel="prettyPhoto"><img class="img-news" src="img/UTCpoint.png" alt="UTC Point"></a></div>
+                <div class="inside">
+                    <hgroup><h2>UTC Point</h2></hgroup>
+                    <div class="entry-content">
+                        <p>Tác giả: Vynk Celeron</p>
+                        <a href="http://diemthigt.96.lt/FTP/UTCpoint/" target="_blank" class="download-link">tải về <i class="fa fa-cloud-download"></i></a>
+                        <p>Phần mềm nhẹ nhàng đơn giản, tự động cập nhật khi có phiên bản mới.</p>
+                    </div>
+                </div>
+            </article>
+        </div>
+        </article>
+    </div>
+</div>
+</section>
+<hr>
